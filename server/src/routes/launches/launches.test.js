012d656fc20008359951d6d74552c43ce4edc3a4
Maryen <voujum@gmail.com>
2023-06-23 12:39:05 +0200

    Closing mongodb connection

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -10,9 +10,9 @@ describe('Launches API', () => {
       await loadPlanetsData();
     });
 
-    // afterAll(async () => {
-    //   await mongoDisconnect();
-    // });
+    afterAll(async () => {
+      await mongoDisconnect();
+    });
 
     describe('Test GET /launches', () => {
       test('It should respond with 200 success', async () => {
